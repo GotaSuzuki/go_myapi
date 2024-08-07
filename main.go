a25e479d87b8e618eba5e65a6511b91d84d0b699
suzuki.gota <suzuki.gota@opst.co.jp>
2024-08-07 15:13:31 +0900

    first

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/hello", handlers.helloHandler)
+	http.HandleFunc("/article", handlers.postArticleHandler)
+	http.HandleFunc("/article/list", handlers.articleListHandler)
+	http.HandleFunc("/article/1", handlers.articleDetailHandler)
+	http.HandleFunc("/article/nice", handlers.postNiceHandler)
+	http.HandleFunc("/comment",handlers.postCommentHandler)
+
+	log.Println("server start at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
